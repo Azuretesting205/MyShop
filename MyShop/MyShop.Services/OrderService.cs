0e57ee22adafaec35e2bab13ca4a3a2694b3cbd0
Azuretesting205 <azuretesting205@gmail.com>
2021-07-11 23:41:05 +0530

    order and security changes

--- MyShop/MyShop.Services/OrderService.cs
+++ MyShop/MyShop.Services/OrderService.cs
@@ -34,5 +34,21 @@ namespace MyShop.Services
             OrderContext.Insert(baseOrder);
             OrderContext.Commit();
         }
+
+        public List<Order> GetOrderList()
+        {
+            return OrderContext.Collection().ToList();
+        }
+
+        public Order GetOrder(string Id)
+        {
+            return OrderContext.Find(Id);
+        }
+
+        public void UpdateOrder(Order order)
+        {
+            OrderContext.Update(order);
+            OrderContext.Commit();
+        }
     }
 }
